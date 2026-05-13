4e13b97593a39abad24fef26162b343ea86fd389
BlanxxtyIS <blanxxtyios@mail.ru>
2026-05-13 16:50:01 +0300

    test vercel

--- frontend/src/components/courses/TopicsPage.tsx
+++ frontend/src/components/courses/TopicsPage.tsx
@@ -6,6 +6,7 @@ import { useAuth } from '../../context/AuthContext'
 import StarField from '../StarField'
 import PremiumGlow from '../PremiumGlow'  
 
+//Sanya
 interface Topic {
   id: number
   title: string
